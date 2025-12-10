2090932b2274078ab3a6e99c9e02e439acb35d02
Maksym Kuznietsov <maksimmanzar4@gmail.com>
2025-12-11 00:02:55 +0100

    message

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,25 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      state === 'fulfilled' ? resolve(delay) : reject(delay);
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then((d) => iziToast.success({ title: 'OK', message: `✅ Fulfilled promise in ${d}ms`, position: 'topRight' }))
+    .catch((d) => iziToast.error({ title: 'Error', message: `❌ Rejected promise in ${d}ms`, position: 'topRight' }));
+
+  form.reset();
+});
